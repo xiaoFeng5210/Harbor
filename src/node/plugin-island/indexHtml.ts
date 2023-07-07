749f821b21c8c513ae9ffea86de152970a1c59de
Eadweard <804875728@qq.com>
2023-07-07 10:54:38 +0800

    优化模版

--- src/node/plugin-island/indexHtml.ts
+++ src/node/plugin-island/indexHtml.ts
@@ -1,19 +1,29 @@
-import { Plugin } from 'vite'
-import { readFile } from 'fs/promises'
-import { DEFAULT_TEMPLATE_PATH } from '../constants'
+// node/plugin-island/indexHtml.ts
+import { readFile } from "fs/promises";
+import { Plugin } from "vite";
+import { DEFAULT_HTML_PATH } from "../constants";
+
 export function pluginIndexHtml(): Plugin {
   return {
-    name: 'island:index-html',
+    name: "island:index-html",
     configureServer(server) {
       return () => {
         server.middlewares.use(async (req, res, next) => {
-          //  读取template.html
-          // 响应template.html
-          const content = await readFile(DEFAULT_TEMPLATE_PATH, "utf-8");
-          res.setHeader('Content-Type', 'text/html')
-          res.end(content)
-        })
-      }
-    }
-  }
+          let html = await readFile(DEFAULT_HTML_PATH, "utf-8");
+          try {
+            html = await server.transformIndexHtml(
+              req.url,
+              html,
+              req.originalUrl
+            );
+            res.statusCode = 200;
+            res.setHeader("Content-Type", "text/html");
+            res.end(html);
+          } catch (e) {
+            return next(e);
+          }
+        });
+      };
+    },
+  };
 }
